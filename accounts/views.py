6e723fc2da5465f26b62c80de3b4aee74331c790
Bryan Kariuki <webdeveloperjoekariuki@gmail.com>
2026-06-30 11:23:54 +0300

    okay

---
+++ accounts/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, redirect
+
+from .forms import RegisterForm
+
+
+def register(request):
+
+    if request.method == "POST":
+
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("login")
+
+    else:
+
+        form = RegisterForm()
+
+    return render(
+        request,
+        "registration/register.html",
+        {
+            "form": form,
+        },
+    )
--- config/urls.py
+++ config/urls.py
@@ -18,11 +18,27 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("core.urls")),
     path("mixes/", include("music.urls")),
     path("booking/", include("booking.urls")),
+    path("accounts/", include("accounts.urls")),
+      # Authentication
+    path(
+        "accounts/login/",
+        auth_views.LoginView.as_view(
+            template_name="registration/login.html"
+        ),
+        name="login",
+    ),
+
+    path(
+        "accounts/logout/",
+        auth_views.LogoutView.as_view(),
+        name="logout",
+    ),
 ]
 
 if settings.DEBUG:
