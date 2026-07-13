510b96935facdde372213f4cbcbd0ee5ae2b5cb1
Bryan Kariuki <webdeveloperjoekariuki@gmail.com>
2026-07-14 00:30:01 +0300

    changes

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,16 +22,26 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j1xzm*fet9a=c@y2ws8h_!y%mjiic_*nh7gg*8y%(@oe%m2wup'
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "django-insecure-j1xzm*fet9a=c@y2ws8h_!y%mjiic_*nh7gg*8y%(@oe%m2wup"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [
-  
- "dj-choppa-xl.onrender.com",
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost,dj-choppa-xl.onrender.com"
+).split(",")
 
-]
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+SESSION_COOKIE_SECURE = not DEBUG
+
+CSRF_COOKIE_SECURE = not DEBUG
+
+SECURE_SSL_REDIRECT = not DEBUG
 
 
 # Application definition
@@ -129,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
@@ -148,3 +160,5 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 LOGIN_URL = "/accounts/login/"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
