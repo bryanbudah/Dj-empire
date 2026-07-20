3331ec5ee8cc2ca5dea8bf929afa6a9e96d6e48c
Bryan Kariuki <webdeveloperjoekariuki@gmail.com>
2026-07-20 09:29:15 +0300

    changes

--- config/urls.py
+++ config/urls.py
@@ -1,25 +1,6 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from django.conf.urls import handler404, handler500
 from django.contrib.sitemaps.views import sitemap
 from core.sitemaps import StaticViewSitemap
 
@@ -33,7 +14,6 @@ urlpatterns = [
     path("mixes/", include("music.urls")),
     path("booking/", include("booking.urls")),
     path("accounts/", include("accounts.urls")),
-      # Authentication
     path(
         "accounts/login/",
         auth_views.LoginView.as_view(
@@ -41,7 +21,6 @@ urlpatterns = [
         ),
         name="login",
     ),
-
     path(
         "accounts/logout/",
         auth_views.LogoutView.as_view(),
@@ -49,22 +28,14 @@ urlpatterns = [
     ),
     path("dashboard/", include("dashboard.urls")),
     path("reviews/", include("reviews.urls")),
-   
+    path("events/", include("events.urls")),
     path(
-    "events/",
-    include("events.urls"),
-),
-
- path(
         "sitemap.xml",
         sitemap,
         {"sitemaps": sitemaps},
         name="django.contrib.sitemaps.views.sitemap",
     ),
 ]
-
-
-
 
 handler404 = "core.views.custom_404"
 handler500 = "core.views.custom_500"
