360e79bbaf54a4ccbc30ae942c05a8cadbfd1368
Bryan Kariuki <webdeveloperjoekariuki@gmail.com>
2026-07-22 08:55:39 +0300

    changes

--- config/settings.py
+++ config/settings.py
@@ -182,13 +182,7 @@ LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "/accounts/login/"
 
 # Email settings for password reset
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
+
 
 # ------------------------------------------------------------------
 # DEFAULT PRIMARY KEY
